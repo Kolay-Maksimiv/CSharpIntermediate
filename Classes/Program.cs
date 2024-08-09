0ae43d39c618a5e410f835719dfd58f54e0da0e8
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2024-08-09 11:06:37 +0300

    Add project files.

---
+++ Classes/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Classes
+{
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}", to, Name);
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "John";
+            person.Introduce("Mosh");
+        }
+    }
+}
