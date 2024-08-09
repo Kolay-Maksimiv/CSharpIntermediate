5988aa8ddc0935a82e881e833c8efcd474834fcb
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2024-08-09 11:30:29 +0300

    IntroductionToClassesProgram

---
+++ Classes/IntroductionToClasses.cs
@@ -0,0 +1,33 @@
+﻿namespace Classes
+{
+    public static class IntroductionToClasses
+    {
+        public static void IntroductionToClassesProgram()
+        {
+            //var person = new Person();
+            //person.Name = "John";
+            //person.Introduce("Mosh");
+
+            var person = Person.Parse("John");
+            person.Introduce("Mosh");
+        }
+    }
+
+    public class Person
+    {
+        public string Name;
+
+        public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, I am {1}.", to, Name);
+        }
+
+        public static Person Parse(string str)
+        {
+            var person = new Person();
+            person.Name = str;
+
+            return person;
+        }
+    }
+}
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,36 +1,12 @@
-﻿using System;
-
-namespace Classes
+﻿namespace Classes
 {
-    public class Person
-    {
-        public string Name;
-
-        public void Introduce(string to)
-        {
-            Console.WriteLine("Hi {0}, I am {1}.", to, Name);
-        }
-
-        public static Person Parse(string str)
-        {
-            var person = new Person();
-            person.Name = str;
-
-            return person;
-        }
-    }
 
     public class Program
     {
         static void Main(string[] args)
         {
-            //var person = new Person();
-            //person.Name = "John";
-            //person.Introduce("Mosh");
-
-
-            var person = Person.Parse("John");
-            person.Introduce("Mosh");
+            //Introduction To Classes
+            IntroductionToClasses.IntroductionToClassesProgram();
         }
     }
 }
