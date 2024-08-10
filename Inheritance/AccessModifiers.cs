f3ced413f19db0533057055917ba6acc35dd8b56
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2024-08-10 16:37:15 +0300

    Access Modifiers Inheritance

---
+++ Inheritance/AccessModifiers.cs
@@ -0,0 +1,41 @@
+﻿namespace Inheritance
+{
+    public static class AccessModifiers
+    {
+        public class Customer
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+
+            public void Promote()
+            {
+                var rating = CulculateRating(excludeOrders: true);
+
+                if (rating == 0)
+                    Console.WriteLine("Promoted to Level 1");
+                else
+                    Console.WriteLine("Promoted to Level 2");
+            }
+
+            protected int CulculateRating(bool excludeOrders)
+            {
+                return 0;
+            }
+        }
+
+        public class GoldCustomer : Customer
+        {
+            public void OfferVouchar()
+            {
+                var rating = CulculateRating(excludeOrders: true);
+            }
+        }
+
+        public static void AccessModifiersProgram()
+        {
+            var goldCustomer = new GoldCustomer();
+
+            goldCustomer.OfferVouchar();
+        }
+    }
+}
--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -4,6 +4,8 @@
     {
         static void Main(string[] args)
         {
+            //Access Modifiers
+            AccessModifiers.AccessModifiersProgram();
         }
     }
 }
