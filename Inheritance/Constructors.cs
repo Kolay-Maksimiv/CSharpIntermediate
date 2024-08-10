45df2df0a84ee00cd64e4dfe951a73234565563f
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2024-08-10 17:05:37 +0300

    Constructors Inheritance

--- Inheritance/Constructors.cs
+++ Inheritance/Constructors.cs
@@ -2,8 +2,35 @@
 {
     public static class Constructors
     {
+        public class Vehicle
+        {
+            private readonly string _registarionNumber;
+
+            //public Vehicle()
+            //{
+            //    Console.WriteLine("Vehicle is being initialized.");
+            //}
+
+            public Vehicle(string registarionNumber)
+            {
+                _registarionNumber = registarionNumber;
+
+                Console.WriteLine("Vehicle is being initialized. {0}", registarionNumber);
+            }
+        }
+
+        public class Car : Vehicle
+        {
+            public Car(string registarionNumber)
+                : base(registarionNumber)
+            {
+                Console.WriteLine("Car is being initialized. {0}", registarionNumber);
+            }
+        }
         public static void ConstructorsProgram()
         {
+            var car = new Car("XYZ1234");
+
             Console.WriteLine("-----------------------------------------------------------------------");
         }
     }
