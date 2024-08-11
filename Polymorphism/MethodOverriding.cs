bddfb5827b66f198957fc6daf4feb1c1061a2e20
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2024-08-11 11:46:50 +0300

    Method Overriding

--- Polymorphism/MethodOverriding.cs
+++ Polymorphism/MethodOverriding.cs
@@ -8,19 +8,47 @@ namespace Polymorphism
             public int Width { get; set; }
             public int Height { get; set; }
             public Position Position { get; set; }
-            public ShapeType Type { get; set; }
+            //public ShapeType Type { get; set; }
+
+            public virtual void Draw()
+            {
+            }
         }
 
         public class Position
         {
         }
 
-        public enum ShapeType
+        public class Circle : Shape
         {
-            Circle,
-            Rectangle,
-            Triangle
+            public override void Draw()
+            {
+                Console.WriteLine("Draw a circle");
+            }
         }
+
+        public class Rectangle : Shape
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("Draw a rectangle");
+            }
+        }
+
+        public class Triangle : Shape
+        {
+            public override void Draw()
+            {
+                Console.WriteLine("Draw a triangle");
+            }
+        }
+
+        //public enum ShapeType
+        //{
+        //    Circle,
+        //    Rectangle,
+        //    Triangle
+        //}
 
         public class Canvas
         {
@@ -28,25 +56,37 @@ namespace Polymorphism
             {
                 foreach (var shape in shapes)
                 {
-                    switch (shape.Type)
-                    {
-                        case ShapeType.Circle:
-                            Console.WriteLine("Draw a circle");
-                            break;
+                    //switch (shape.Type)
+                    //{
+                    //    case ShapeType.Circle:
+                    //        Console.WriteLine("Draw a circle");
+                    //        break;
 
-                        case ShapeType.Rectangle:
-                            Console.WriteLine("Draw a rectangle");
-                            break;
-                        case ShapeType.Triangle:
-                            Console.WriteLine("Draw a triangle");
-                            break;
-                    }
+                    //    case ShapeType.Rectangle:
+                    //        Console.WriteLine("Draw a rectangle");
+                    //        break;
+                    //    case ShapeType.Triangle:
+                    //        Console.WriteLine("Draw a triangle");
+                    //        break;
+                    //}
+
+                    shape.Draw();
                 }
             }
         }
 
         public static void MethodOverridingProgram()
         {
+            var shapes = new List<Shape>();
+
+            //shapes.Add(new Shape { Width = 100, Height = 100, Type = ShapeType.Circle });
+            //shapes.Add(new Shape { Width = 100, Height = 30, Type = ShapeType.Rectangle });
+            shapes.Add(new Circle());
+            shapes.Add(new Rectangle());
+            shapes.Add(new Triangle());
+
+            var convas = new Canvas();
+            convas.DrawShapes(shapes);
 
             Console.WriteLine("-----------------------------------------------------------------------");
         }
