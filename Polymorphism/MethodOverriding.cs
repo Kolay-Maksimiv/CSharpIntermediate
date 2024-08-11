fc2750107afb20e2fb226a9b50d4ecc594949a0c
Mykola Maksymiv <kolaymaksimiv@gmail.com>
2024-08-11 11:24:35 +0300

    base code

---
+++ Polymorphism/MethodOverriding.cs
@@ -0,0 +1,54 @@
+﻿
+namespace Polymorphism
+{
+    public static class MethodOverriding
+    {
+        public class Shape
+        {
+            public int Width { get; set; }
+            public int Height { get; set; }
+            public Position Position { get; set; }
+            public ShapeType Type { get; set; }
+        }
+
+        public class Position
+        {
+        }
+
+        public enum ShapeType
+        {
+            Circle,
+            Rectangle,
+            Triangle
+        }
+
+        public class Canvas
+        {
+            public void DrawShapes(List<Shape> shapes)
+            {
+                foreach (var shape in shapes)
+                {
+                    switch (shape.Type)
+                    {
+                        case ShapeType.Circle:
+                            Console.WriteLine("Draw a circle");
+                            break;
+
+                        case ShapeType.Rectangle:
+                            Console.WriteLine("Draw a rectangle");
+                            break;
+                        case ShapeType.Triangle:
+                            Console.WriteLine("Draw a triangle");
+                            break;
+                    }
+                }
+            }
+        }
+
+        public static void MethodOverridingProgram()
+        {
+
+            Console.WriteLine("-----------------------------------------------------------------------");
+        }
+    }
+}
